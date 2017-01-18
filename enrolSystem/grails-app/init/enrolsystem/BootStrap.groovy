a4cffb94af01610ec9f50b96275eb0f5667e4d3c
lukegreenwoodgit <luke.greenwood@student.shu.ac.uk>
2017-01-18 11:22:52 +0000

    Created Lecturer and Module Classes

--- enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
+++ enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
@@ -1,9 +1,37 @@
-package enrolsystem
+package com.enrol
 
 class BootStrap {
 
     def init = { servletContext ->
+	def computing = new Course(
+	title:'BSc Hon Computing',
+	code:'COMP234',
+	leader:'Dr Mary Poppins',
+	department:'Computing',
+	startDate:new Date('11/11/2016'),
+	endDate:new Date('11/11/2020'),
+	numberOfStudents:45,
+	description:'''Computing Course at Sheffield Hallam University''',
+	fees:9000,
+	studyMode:'Full-time'
+	).save()
+	def module = new Module(
+	title:'System Architectures',
+	code:'COMP145',
+	credits:20,
+	lecturer:'Tonderai',
+	description:'''System Architectures module for Computing Course''',
+	course:'computing'
+	).save()
     }
     def destroy = {
+	def lectruer = new Lectruer(
+	fullName:'Dr Tonderai Maswera',
+	post:'Lecturer',
+	subject:'Web Applications',
+	lecturerEmail:'t.maswera@shu.ac.uk',
+	office:'Cantor-9417',
+	bio:'''I am Tonderai'''
+	).save()
     }
 }
